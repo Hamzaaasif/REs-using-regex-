c8df9a853327bb7121fd8d138e92d1bba9d2b874
Hamza Asif <hamzaasif0987@gmail.com>
2019-09-03 01:07:46 +0500

    completed !

--- main.cpp
+++ main.cpp
@@ -8,16 +8,17 @@ int main()
   string intconst="[+|-][[:digit:]]+|[0-9]+";
   string floatconst="[+|-][0-9]*\\.[0-9]+|[0-9]*\\.[0-9]+";
 
-  string charA="[nortb]";
-  string charB="";
-  string charC="";
-  string charconst="";
+  string charA ="\\'[\\\\][nortb]\\'";
+  string charB="\\'[\\\\][\\'\\\"\\\\]\\'";
+  string charC="\\'[!@#$%^&*()_=+-|:;,.?}~`{]\\'";
+  string charConst=charC+"|"+charA+"+|"+charB+"|"+charA;
  while(true)
   {
     cout<<"Enter string to check Validity: "<<endl;
     cin >> input;
-    regex regstr(intconst);
+    regex regstr(charConst);
     bool match = regex_match(input,regstr);
+
     cout<<(match?"Valid " : "Not valid ")<<endl<<endl;
   }
   system("pause");
